374fed8707ac7455443b04473236d2c848895735
Walter Carrasco <carrascowalter440@gmail.com>
2022-12-30 22:03:59 -0300

    changed bg

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ function newElement() {
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
   if (inputValue === '') {
-    alert("You must write something!");
+    alert("Tienes que escribir Algo ");
   } else {
     document.getElementById("myUL").appendChild(li);
   }
